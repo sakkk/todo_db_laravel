f9c0199f3558565151d5ccf48740f367c0709540
sakkk <yusaku.katsuki@gmail.com>
2021-07-23 12:49:27 +0900

    mod AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (\App::envitonment(['production']))
+        {
+            \URL::forceSceme('https');
+        }
     }
 }
